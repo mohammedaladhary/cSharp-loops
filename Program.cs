450672c78b7eeb5f5876b9021b9dc722be4a76f5
mohammedaladhary <caladhary@gmail.com>
2023-06-04 15:04:23 +0400

    odd/even number

--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        int a = 1;
-        Console.WriteLine(a++); // a= 1 . post increament
-        // now after the post increament is a =2
-        Console.WriteLine(++a); // a= 3 . pre increament
+        //int a = 1;
+        //Console.WriteLine(a++); // a= 1 . post increament
+        //// now after the post increament is a =2
+        //Console.WriteLine(++a); // a= 3 . pre increament
 
 
         //for (int i = 0; i <= 5; i++)
@@ -34,6 +34,33 @@
         //    Console.WriteLine(a);
         //    //a++;
         //    //a = a + 5;
+        //}
+
+        Console.WriteLine("enter a number :");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("enter a second number:");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Odd numbers are :");
+
+        for (int i=a = 1; i <= b; i+=2)
+        {
+            Console.WriteLine(i);
+        }
+
+        //{
+        //    int n;
+
+        //    Console.WriteLine("Please enter a number : ");
+        //    n = Convert.ToInt32(Console.ReadLine());
+
+        //    Console.WriteLine("Factorial of the number is : ");
+
+        //    for (int i = n - 1; i > 0; i--)
+        //        {
+        //            n *= i;
+        //        }
         //}
 
     }
