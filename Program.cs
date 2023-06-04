e943625b5fac0e7c61dc0517c0a9e180b55f4b99
mohammedaladhary <caladhary@gmail.com>
2023-06-04 12:33:13 +0400

    pre/post increament

--- Program.cs
+++ Program.cs
@@ -2,18 +2,39 @@
 {
     private static void Main(string[] args)
     {
-        for (int i = 0; i <= 5; i++)
-        {
-            Console.Write("*");
-        }
-        Console.WriteLine();
-        for (int i = 0; i <= 5; i++)
-        {
-            Console.WriteLine("*    *");
-        }
-        for (int i = 0; i <= 5; i++)
-        {
-            Console.Write("*");
-        }
+        int a = 1;
+        Console.WriteLine(a++); // a= 1 . post increament
+        // now after the post increament is a =2
+        Console.WriteLine(++a); // a= 3 . pre increament
+
+
+        //for (int i = 0; i <= 5; i++)
+        //{
+        //    Console.Write("*");
+        //}
+        //Console.WriteLine();
+        //for (int i = 0; i <= 5; i++)
+        //{
+        //    Console.WriteLine("*    *");
+        //}
+        //for (int i = 0; i <= 5; i++)
+        //{
+        //    Console.Write("*");
+        //}
+
+        //int a = 2;
+
+        //do { 
+        //    Console.WriteLine(a);
+        //    a++; 
+        //} while (a <= 5);
+
+        //while (a >1)
+        //{
+        //    Console.WriteLine(a);
+        //    //a++;
+        //    //a = a + 5;
+        //}
+
     }
 }
