c8f3d3968a1ac65e81ac41a32582aa32aa20f3f9
mohammedaladhary <caladhary@gmail.com>
2023-06-05 14:57:05 +0400

    print stars

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿internal class Program
+﻿using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -36,18 +40,18 @@
         //    //a = a + 5;
         //}
 
-        Console.WriteLine("enter a number :");
-        int a = int.Parse(Console.ReadLine());
+        //Console.WriteLine("enter a number :");
+        //int a = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("enter a second number:");
-        int b = int.Parse(Console.ReadLine());
+        //Console.WriteLine("enter a second number:");
+        //int b = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Odd numbers are :");
+        //Console.WriteLine("Odd numbers are :");
 
-        for (int i=a = 1; i <= b; i+=2)
-        {
-            Console.WriteLine(i);
-        }
+        //for (int i=a = 1; i <= b; i+=2)
+        //{
+        //    Console.WriteLine(i);
+        //}
 
         //{
         //    int n;
@@ -62,6 +66,50 @@
         //            n *= i;
         //        }
         //}
+        //-------------------------------------------------
+
+        //Console.WriteLine("enter a number :");
+        //int a = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine("enter a second number:");
+        //int b = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine("Odd/even numbers are :");
+
+        //for (int i = a ; i <= b; i ++)
+        //{
+        //    if (i % 2 == 0)
+        //    {
+        //        Console.WriteLine(i + " - even");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine(i + "- odd");
+        //    }
+        //}
+        //        enter a number:
+        //        2
+        //enter a second number:
+        //5
+        //Odd / even numbers are :
+        //2 - even
+        //3 - odd
+        //4 - even
+        //5 - odd
+        //--------------------------------------
+
+        Console.WriteLine("enter a number of rows :");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        for (int i = 1; i <= a; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
 
     }
 }
